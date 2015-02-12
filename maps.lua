2ea40fd7e5559b6b6cbfe599fbbd1a28bca6b22d
Jonas Lieb <lieb.jonas@gmail.com>
2015-02-12 20:28:18 +0100

    changed sizes to reflect realistic sizes in m

--- maps.lua
+++ maps.lua
@@ -1,50 +1,50 @@
 map1 = {
-	bounds = {{0,0},{22,16}},
+	bounds = {{0,0},{2200,1600}},
 	inbound = {"airIncoming"},
 	
 	nodes = {
 		["gate1"] = {
-			pos = {10,13},
+			pos = {1000,1300},
 			nxt = {"crossing"},
 		},
 		["gate2"] = {
-			pos = {13,13},
+			pos = {1300,1300},
 			nxt = {"crossing"},
 		},
 		["crossing"] = {
-			pos = {10,9},
+			pos = {1000,900},
 			nxt = {"fwdTakeoff1"},
 		},
 		["fwdTakeoff1"] = {
-			pos = {10,2},
+			pos = {1000,200},
 			nxt = {"fwdTakeoff2"},
 		},
 		["fwdTakeoff2"] = {
-			pos = {2,2},
+			pos = {200,200},
 			nxt = {"takeoffQueue"},
 		},
 		["takeoffQueue"] = {
-			pos = {2,6},
+			pos = {200,600},
 			nxt = {"takeoff"},
 		},
 		["takeoff"] = {
-			pos = {4,6},
+			pos = {400,600},
 			nxt = {"land"},
 		},
 		["land"] = {
-			pos = {20,6},
+			pos = {2000,600},
 			nxt = {"fwdLand1"},
 		},
 		["fwdLand1"] = {
-			pos = {20,10},
+			pos = {2000,1000},
 			nxt = {"fwdLand2"},
 		}, 
 		["fwdLand2"] = {
-			pos = {15,10},
+			pos = {1500,1000},
 			nxt = {"gate1", "gate2"},
 		},
 		["airIncoming"] = {
-			pos = {-1,6},
+			pos = {-100,600},
 			nxt = {"land"},
 		}
 	}
