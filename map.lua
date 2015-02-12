a208f881d502e6c690c10f00327d35ed6a58b3a0
Jonas Lieb <lieb.jonas@gmail.com>
2015-02-12 20:13:23 +0100

    push/pop while map drawing

--- map.lua
+++ map.lua
@@ -12,25 +12,29 @@ end
 function drawMap(map) 
 	assert(map, "No map defined")
 	
-	local w = map.bounds[2][1] - map.bounds[1][1]
-	local h = map.bounds[2][2] - map.bounds[1][2]
-	local gw, gh = love.graphics.getDimensions()
-	local scale = math.max(gh/h, gw/w)
-	
-	love.graphics.translate(gw/2, gh/2)
-	love.graphics.scale(scale)
-	love.graphics.translate(-w/2, -h/2)
+	love.graphics.push()
+		
+		local w = map.bounds[2][1] - map.bounds[1][1]
+		local h = map.bounds[2][2] - map.bounds[1][2]
+		local gw, gh = love.graphics.getDimensions()
+		local scale = math.min(gh/h, gw/w)
+		
+		love.graphics.translate(gw/2, gh/2)
+		love.graphics.scale(scale)
+		love.graphics.translate(-w/2, -h/2)
 
-	local nodes = map.nodes
+		local nodes = map.nodes
 
-	love.graphics.setColor({0,255,0})
-	for name, node in pairs(nodes) do
-		love.graphics.setLineWidth(0.1)
-		love.graphics.circle("fill", node.pos[1], node.pos[2], 0.2, 16)
-		for i=1,#node.nxt do
-			local nxtPos = nodes[node.nxt[i]].pos
-			love.graphics.line(node.pos[1], node.pos[2], nxtPos[1], nxtPos[2])
+		love.graphics.setColor({0,255,0})
+		for name, node in pairs(nodes) do
+			love.graphics.setLineWidth(0.1)
+			love.graphics.circle("fill", node.pos[1], node.pos[2], 0.2, 16)
+			for i=1,#node.nxt do
+				local nxtPos = nodes[node.nxt[i]].pos
+				love.graphics.line(node.pos[1], node.pos[2], nxtPos[1], nxtPos[2])
+			end
 		end
-	end
+	
+	love.graphics.pop()
 	
 end
