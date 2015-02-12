26b129617d0d33abc66b7d4d800f01eff27ce1bb
joelschum@gmail.com <joelschum@gmail.com>
2015-02-12 23:42:50 +0100

    finished basic ui

--- map.lua
+++ map.lua
@@ -16,7 +16,7 @@ function drawMap(map)
 		
 		local w = map.bounds[2][1] - map.bounds[1][1]
 		local h = map.bounds[2][2] - map.bounds[1][2]
-		local gw, gh = love.graphics.getDimensions()
+		local gw, gh = getGameViewDimensions()
 		local scale = math.min(gh/h, gw/w)
 		
 		love.graphics.translate(gw/2, gh/2)
