8e99080bf3330e838e13ad9e6d2ed6fe42657831
joelschum@gmail.com <joelschum@gmail.com>
2015-02-13 21:48:38 +0100

    Added actions for new map

--- actions.lua
+++ actions.lua
@@ -13,7 +13,7 @@ actions = {
 	},
 	["takeoff"] = {
 		name = "Take off",
-		message = "Permission to take off",
+		message = "Runway cleared for takeoff",
 		shortcut = "o"
 	},
 	["gate1"] = {
@@ -25,5 +25,65 @@ actions = {
 		name = "To gate 2",
 		message = "Permission to continue to gate 2",
 		shortcut = "j"
+	},
+	["gate3"] = {
+		name = "To gate 3",
+		message = "Permission to continue to gate 3",
+		shortcut = ,
+	},
+	["taxiStart1"] = {
+		name = "To runway 1",
+		message = "Permission to taxi to runway 1",
+		shortcut = ,
+	},
+	["taxiStart2"] = {
+		name = "To runway 2",
+		message = "Permission to taxi to runway 2",
+		shortcut = ,
+	},
+	["taxiStart3"] = {
+		name = "To runway 3",
+		message = "Permission to taxi to runway 3",
+		shortcut = ,
+	},
+	["taxiGatesN"] = {
+		name = "To northern gates",
+		message = "Proceed to northern gates",
+		shortcut = ,
+	},
+	["taxiGatesS"] = {
+		name = "To southern gates",
+		message = "Proceed to southern gates",
+		shortcut = ,
+	},
+	["changeToRunway1"] = {
+		name = "Change to runway 1",
+		message = "Cancel approach, change to runway 1",
+		shortcut = ,
+	},
+	["changeToRunway2"] = {
+		name = "Change to runway 2",
+		message = "Cancel approach, change to runway 2",
+		shortcut = ,
+	},
+	["changeToRunway3"] = {
+		name = "Change to runway 3",
+		message = "Cancel approach, change to runway 3",
+		shortcut = ,
+	},
+	["holdForRunway1"] = {
+		name = "Hold for touchdown",
+		message = "Cancel approach, proceed with holding pattern",
+		shortcut = ,
+	},
+	["holdForRunway2"] = {
+		name = "Hold for touchdown",
+		message = "Cancel approach, proceed with holding pattern",
+		shortcut = ,
+	},
+	["holdForRunway3"] = {
+		name = "Hold for touchdown",
+		message = "Cancel approach, proceed with holding pattern",
+		shortcut = ,
 	},
 }
