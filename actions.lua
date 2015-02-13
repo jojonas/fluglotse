fd69c5c7a6f7e4cd346f6fd2e5eb249e1f0c0712
joelschum@gmail.com <joelschum@gmail.com>
2015-02-13 21:54:57 +0100

    added shortcuts

--- actions.lua
+++ actions.lua
@@ -4,86 +4,86 @@ actions = {
 	["taxi"] = {
 		name = "Taxi to runway",
 		message = "Permission to taxi to runway",
-		shortcut = "t"
+		shortcut = "q"
 	},
 	["cross"] = {
 		name = "Cross runway",
 		message = "Permission to cross runway",
-		shortcut = "c"
+		shortcut = "w"
 	},
 	["takeoff"] = {
 		name = "Take off",
 		message = "Runway cleared for takeoff",
-		shortcut = "o"
+		shortcut = "e"
 	},
 	["gate1"] = {
 		name = "To gate 1",
 		message = "Permission to continue to gate 1",
-		shortcut = "u"
+		shortcut = "r"
 	},
 	["gate2"] = {
 		name = "To gate 2",
 		message = "Permission to continue to gate 2",
-		shortcut = "j"
+		shortcut = "t"
 	},
 	["gate3"] = {
 		name = "To gate 3",
 		message = "Permission to continue to gate 3",
-		shortcut = ,
+		shortcut = "a",
 	},
 	["taxiStart1"] = {
 		name = "To runway 1",
 		message = "Permission to taxi to runway 1",
-		shortcut = ,
+		shortcut = "s",
 	},
 	["taxiStart2"] = {
 		name = "To runway 2",
 		message = "Permission to taxi to runway 2",
-		shortcut = ,
+		shortcut = "d",
 	},
 	["taxiStart3"] = {
 		name = "To runway 3",
 		message = "Permission to taxi to runway 3",
-		shortcut = ,
+		shortcut = "f",
 	},
 	["taxiGatesN"] = {
 		name = "To northern gates",
 		message = "Proceed to northern gates",
-		shortcut = ,
+		shortcut = "g",
 	},
 	["taxiGatesS"] = {
 		name = "To southern gates",
 		message = "Proceed to southern gates",
-		shortcut = ,
+		shortcut = "h",
 	},
 	["changeToRunway1"] = {
 		name = "Change to runway 1",
 		message = "Cancel approach, change to runway 1",
-		shortcut = ,
+		shortcut = "q",
 	},
 	["changeToRunway2"] = {
 		name = "Change to runway 2",
 		message = "Cancel approach, change to runway 2",
-		shortcut = ,
+		shortcut = "w",
 	},
 	["changeToRunway3"] = {
 		name = "Change to runway 3",
 		message = "Cancel approach, change to runway 3",
-		shortcut = ,
+		shortcut = "e",
 	},
 	["holdForRunway1"] = {
 		name = "Hold for touchdown",
 		message = "Cancel approach, proceed with holding pattern",
-		shortcut = ,
+		shortcut = "a",
 	},
 	["holdForRunway2"] = {
 		name = "Hold for touchdown",
 		message = "Cancel approach, proceed with holding pattern",
-		shortcut = ,
+		shortcut = "s",
 	},
 	["holdForRunway3"] = {
 		name = "Hold for touchdown",
 		message = "Cancel approach, proceed with holding pattern",
-		shortcut = ,
+		shortcut = "d",
 	},
 }
