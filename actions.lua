137bf8aed5102c17e955bd7af7445c79e927b056
Jonas Lieb <lieb.jonas@gmail.com>
2015-02-13 01:07:53 +0100

    added action definitions

---
+++ actions.lua
@@ -0,0 +1,29 @@
+actionOrder = {"taxi", "cross", "takeoff", "gate1", "gate2"}
+
+actions = {
+	["taxi"] = {
+		name = "Taxi to runway",
+		message = "Permission to taxi to runway",
+		shortcut = "t"
+	},
+	["cross"] = {
+		name = "Cross runway",
+		message = "Permission to cross runway",
+		shortcut = "c"
+	},
+	["takeoff"] = {
+		name = "Take off",
+		message = "Permission to take off",
+		shortcut = "o"
+	},
+	["gate1"] = {
+		name = "To gate 1",
+		message = "Permission to continue to gate 1",
+		shortcut = "u"
+	},
+	["gate2"] = {
+		name = "To gate 2",
+		message = "Permission to continue to gate 2",
+		shortcut = "j"
+	},
+}
